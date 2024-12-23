b956aa38f666e0961fbf82cbdd6959130c600d88
SjostromJesper <j.sjostrom@live.se>
2024-12-23 22:21:07 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import {createServer} from 'node:http'
+import { Server } from "socket.io"
+
+const app = express();
+
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('A user connected')
+
+    socket.on('message', (message) => {
+        console.log('message:', message)
+        sendToAll(message)
+    })
+})
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000')
+});
+
+const sendToAll = (message) => {
+    console.log(message)
+
+    io.emit('message', message)
+}
