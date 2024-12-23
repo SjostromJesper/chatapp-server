b0a427baccbea93eaf1250b45b60b4cf547f16a2
SjostromJesper <j.sjostrom@live.se>
2024-12-23 23:31:38 +0100

    added username to participants

--- server.js
+++ server.js
@@ -15,9 +15,18 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
     console.log('A user connected')
 
+    const username = socket.handshake.query.username || 'anon'
+
+    socket.username = username
+
+    socket.broadcast.emit('message', {
+        username: 'Server',
+        message: `${username} has joined the chat!`
+    });
+
     socket.on('message', (message) => {
         console.log('message:', message)
-        sendToAll(message)
+        sendToAll(username, message)
     })
 })
 
@@ -25,8 +34,8 @@ server.listen(3000, () => {
     console.log('Server started on port 3000')
 });
 
-const sendToAll = (message) => {
+const sendToAll = (username, message) => {
     console.log(message)
 
-    io.emit('message', message)
+    io.emit('message', {username, message})
 }
